e2fae07f63c569c15f74eace96905c7ab6506c90
RyanTseng <ryan10328@gmail.com>
2018-05-22 15:20:10 +0800

    animation lab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('state', [
+      state('increase', style({})),
+      state('decrease', style({})),
+      transition(
+        '* => increase',
+        animate(
+          '0.5s',
+          keyframes([
+            style({ backgroundColor: 'transparent' }),
+            style({ backgroundColor: 'green' }),
+            style({ backgroundColor: 'transparent' })
+          ])
+        )
+      ),
+      transition(
+        '* => decrease',
+        animate(
+          '0.5s',
+          keyframes([
+            style({ backgroundColor: 'transparent' }),
+            style({ backgroundColor: 'red' }),
+            style({ backgroundColor: 'transparent' })
+          ])
+        )
+      )
+    ])
+  ]
 })
 export class AppComponent {
   title = 'app';
+  num = 0;
+  state = 'n/a';
+
+  add() {
+    this.state = 'increase';
+    this.num = this.num + 1;
+  }
+
+  minus() {
+    this.state = 'decrease';
+    this.num = this.num - 1;
+  }
 }
